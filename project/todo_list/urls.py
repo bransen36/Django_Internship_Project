0994935303a981c0ba565cbf4163baf6bb321a89
bransen36 <bransenerickson02@gmail.com>
2025-05-26 21:51:01 -0600

    Added authentication and edited templates to extend base

--- project/todo_list/urls.py
+++ project/todo_list/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
-    path('sign_up/', views.signup_view, name='sign_up')
+    path('login/', views.login_view.as_view(), name='login'),
+    path('sign_up/', views.signup_view.as_view(), name='sign_up'),
+    path('lists/', views.list_view.as_view(), name='lists'),
+    path('logout/', views.logout_view.as_view(), name='logout')
 ]
--- project/todo_list/views.py
+++ project/todo_list/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm, SignUpForm
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import *
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import *
+from django.views.generic import *
+from django.views import *
 
 
 # Create your views here.
@@ -11,9 +13,32 @@ from django.contrib.auth.models import User
 def index(request):
     return render(request, 'todo_list/index.html')
 
+# Sign Up View
+class signup_view(View):
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, 'todo_list/sign_up.html', {'form': form})
+    
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password1 = form.cleaned_data['password1']
+
+            user = User.objects.create_user(username, email, password1)
+            
+            return render(request, 'todo_list/sign_up_success.html')
+        else:
+            return render(request, 'todo_list/sign_up.html', {'form': form})
+
 # Login View
-def login_view(request):
-    if request.method == 'POST':
+class login_view(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'todo_list/login.html', {'form': form})
+    
+    def post(self, request):
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
@@ -29,24 +54,13 @@ def login_view(request):
         else:
             form = LoginForm()
         return render(request, 'todo_list/login.html', {'form': form})
-    else:
-        form = LoginForm()
-        return render(request, 'todo_list/login.html', {'form': form})
+    
+class logout_view(LoginRequiredMixin, View):
+    login_url = 'todo_list/login.html'
 
-# Sign Up View
-def signup_view(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password1 = form.cleaned_data['password1']
-
-            user = User.objects.create_user(username, email, password1)
-            
-            return render(request, 'todo_list/sign_up_success.html')
-        else:
-            return render(request, 'todo_list/sign_up.html', {'form': form})
-    else:
-        form = SignUpForm()
-        return render(request, 'todo_list/sign_up.html', {'form': form})
+    def get(self, request):
+        logout(request)
+        return render(request, 'todo_list/index.html')
+    
+class list_view(LoginRequiredMixin, TemplateView):
+    template_name = 'todo_list/lists.html'
